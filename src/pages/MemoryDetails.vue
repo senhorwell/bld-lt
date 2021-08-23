0e13634709bed2fe1e4c7ff2f69a96f71c6c5aa5
Well Luiz <wellwlds@hotmail.com>
2021-08-23 20:04:00 -0300

    updating detail screen, showing title on screen

--- src/pages/MemoryDetails.vue
+++ src/pages/MemoryDetails.vue
@@ -1,10 +1,29 @@
 <template>
-    <base-layout page-title="BLD Logistica - Detalhes" page-default-back-link="/memories">
-        <h2>Pagina de detalhes</h2>
+    <base-layout :page-title="loadedMemory ? loadedMemory.title : 'Loading...'" page-default-back-link="/memories">
+        <h2 v-if="!loadedMemory">Não foi encontrado na memoria o id informado.</h2>
+        <h2 v-else>Carregado</h2>
     </base-layout>
 </template>
 
 <script>
 
-export default {}
+export default {
+    data() {
+        return {
+            memoryId: this.$route.params.id,
+        }
+    }, 
+    computed: {
+        loadedMemory() {
+            return this.$store.getters.memory(this.memoryId);
+        },
+    },
+    watch: {
+        $route(currentRoute) {
+            this.memoryId = currentRoute.params.id;
+        },
+    },
+
+}
+
 </script>
