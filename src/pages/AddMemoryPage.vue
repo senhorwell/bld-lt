866be566557491c011aefe57b7cbb3ec7ecdd20a
Well Luiz <wellwlds@hotmail.com>
2021-08-23 20:40:46 -0300

    linking to add new memory screen

---
+++ src/pages/AddMemoryPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <base-layout page-title="Adicionar uma memória" page-default-back-link="/memories">
+        
+    </base-layout>
+</template>
+
+<script>
+import BaseLayout from '@/components/base/BaseLayout.vue'
+export default {
+  components: { BaseLayout },
+    
+}
+</script>
