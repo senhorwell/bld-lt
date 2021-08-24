b3e1f6db06d38471fe67e1e800a3d0bfc0d5140d
Well Luiz <wellwlds@hotmail.com>
2021-08-23 21:06:06 -0300

    add new memory working with image link

---
+++ src/components/memories/CreateMemoryForm.vue
@@ -0,0 +1,53 @@
+<template>
+    <form class="ion-padding" @submit.prevent="submitForm">
+       <ion-list>
+          <ion-item>
+            <ion-label position="floating">Title</ion-label>
+            <ion-input type="text" required v-model="enteredTitle" />
+          </ion-item>
+          <ion-item>
+            <ion-label position="floating">Imagem</ion-label>
+            <ion-input type="url" required v-model="enteredImageUrl" />
+          </ion-item>
+          <ion-item>
+            <ion-label position="floating">Descricao</ion-label>
+            <ion-textarea rows="5" required v-model="enteredDescription"></ion-textarea>
+          </ion-item>
+          <ion-button type="submit" expand="block">Salvar</ion-button>
+        </ion-list>
+    </form>
+</template>
+
+
+<script>
+import { IonList, IonItem, IonLabel, IonInput, IonTextarea,IonButton} from '@ionic/vue';
+
+export default {
+    emits: ['save-memory'],
+    components: {
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonTextarea,
+    IonButton,
+  },
+  data( ) {
+      return {
+          enteredTitle: '',
+          enteredImageUrl: '',
+          enteredDescription: ''
+      };
+  },
+  methods: {
+      submitForm() {
+          const memoryData = {
+              title: this.enteredTitle,
+              imageUrl: this.enteredImageUrl,
+              description: this.enteredDescription,
+          };
+          this.$emit('save-memory',memoryData);
+      },
+  },
+};
+</script>
--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,13 +1,21 @@
 <template>
     <base-layout page-title="Adicionar uma memória" page-default-back-link="/memories">
-        
+    <create-memory-form @save-memory="saveMemory"></create-memory-form>
     </base-layout>
 </template>
 
 <script>
-import BaseLayout from '@/components/base/BaseLayout.vue'
+import CreateMemoryForm from '../components/memories/CreateMemoryForm.vue';
+
 export default {
-  components: { BaseLayout },
-    
+  components: {
+    CreateMemoryForm,
+  },
+  methods: {
+    saveMemory(memoryData) {
+      this.$store.dispatch('addMemory',memoryData);
+      this.$router.replace('/memories');
+    }
+  }
 }
 </script>
